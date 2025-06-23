f363a8f8d40847b169d0b74b4d31bdc862e962e0
Lukas Meinhart <100217325+dowsn@users.noreply.github.com>
2025-06-23 09:52:18 +0000

    tae

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
-from app import views as app_views
 
 # from courses import views
 
